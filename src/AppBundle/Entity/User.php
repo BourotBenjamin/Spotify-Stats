e2269f358256c8e8d3aee8106bdfb5bd5aeb7b02
Benjamin Bourot <bourot.benjamin@gmail.com>
2017-07-27 22:34:20 +0200

    fix typo

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -37,7 +37,7 @@ class User
 
 
     /**
-     * @ORM\Column(name="last_fetch", type="bigint", nullable=ture)
+     * @ORM\Column(name="last_fetch", type="bigint", nullable=true)
      */
     private $last_fetch;
 
